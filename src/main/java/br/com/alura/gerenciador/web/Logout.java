71c5a6a0112f185eb567097ba5b3f1f1b36866f4
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2016-10-09 00:29:49 -0300

    Utilizando redirecionamento server-side

--- src/main/java/br/com/alura/gerenciador/web/Logout.java
+++ src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -1,17 +1,15 @@
 package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.Usuario;
-import br.com.alura.gerenciador.web.Cookies;
 
 @WebServlet(urlPatterns = "/logout")
 public class Logout extends HttpServlet {
@@ -20,15 +18,11 @@ public class Logout extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		Usuario usuario = (Usuario) req.getSession().getAttribute("usuario.logado");
-
-		String mensagem = "Usuário deslogado com sucesso.";
+;
 		if (usuario != null) {
 			req.getSession().removeAttribute("usuario.logado");
-		} else {
-			mensagem = "Usuário não estava logado.";
-		}
-
-		PrintWriter writer = resp.getWriter();
-		writer.write("<html><body>" + mensagem + "</body></html>");
+		} 
+		RequestDispatcher dispatcher = req.getRequestDispatcher("WEB-INF/pages/logout.html");
+		dispatcher.forward(req, resp);
 	}
 }
