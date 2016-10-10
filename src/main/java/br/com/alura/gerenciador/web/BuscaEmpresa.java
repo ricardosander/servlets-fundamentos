12cfdc3ead469d35096c1bb2fb111af809f1f3d3
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2016-10-10 00:42:39 -0300

    Altedando comportamento da classe BuscaEmpresa para novo padrão MVC.

--- src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -1,28 +1,23 @@
 package br.com.alura.gerenciador.web;
 
-import java.io.IOException;
 import java.util.Collection;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.Empresa;
 import br.com.alura.gerenciador.dao.EmpresaDAO;
 
-@WebServlet(urlPatterns = "/busca")
-public class BuscaEmpresa extends HttpServlet {
+public class BuscaEmpresa implements Controller {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public String executa(HttpServletRequest request, HttpServletResponse response) {
 
-		String filtro = req.getParameter("filtro");
+		String filtro = request.getParameter("filtro");
 
 		EmpresaDAO empresaDAO = new EmpresaDAO();
 		Collection<Empresa> empresas = empresaDAO.buscaPorSimilaridade(filtro);
-		req.setAttribute("empresas", empresas);
-		req.getRequestDispatcher("/WEB-INF/pages/buscaEmpresa.jsp").forward(req, resp);
+		request.setAttribute("empresas", empresas);
+		return "/WEB-INF/pages/buscaEmpresa.jsp";
 	}
 }
